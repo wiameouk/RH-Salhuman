fd32dad5f5bdb7ccd5884cdea1df4e2f0a520ab9
manalmsf <149411712+manalmsf@users.noreply.github.com>
2024-05-10 14:51:52 +0100

    commit wiam 2

--- src/main/java/com/example/rhsalhuman/servicesImpl/FicheDepaieServiceImp.java
+++ src/main/java/com/example/rhsalhuman/servicesImpl/FicheDepaieServiceImp.java
@@ -4,11 +4,15 @@ import com.example.rhsalhuman.entities.Conge;
 import com.example.rhsalhuman.entities.FicheDePaie;
 import com.example.rhsalhuman.repositories.FichePaieRepository;
 import com.example.rhsalhuman.services.FicheDePaieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class FicheDepaieServiceImp implements FicheDePaieService {
 
+    @Autowired
     private FichePaieRepository fichePaieRepository;
     @Override
     public FicheDePaie saveFicheDePaie(FicheDePaie ficheDePaie) {
